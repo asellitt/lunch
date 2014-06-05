5d7ab2be5ca2fbba2638f1bf71f5cd47d7781ff1
Anthony Sellitti <anthony.sellitti@envato.com>
2014-06-05 22:27:07 +1000

    list of restaurants and endpoints

--- lunch.rb
+++ lunch.rb
@@ -6,11 +6,80 @@ class Lunch < Sinatra::Base
   set :raise_errors, false
   set :show_exceptions, false
 
-  get '/lunch' do
-    content_type :json
-    {:json => 'string'}.to_json
+  before do
+    content_type 'application/json'
   end
-  
+
+  get '/' do
+    restaurants.to_json
+  end
+
+  get '/random' do
+    random_restaurant(restaurants).to_json
+  end
+
+  get '/within/:distance' do
+    restaurants_within(params[:distance]).to_json
+  end
+
+  get '/within/:distance/random' do
+    random_restaurant(restaurants_within(params[:distance])).to_json
+  end
+
+private
+
+  def random_restaurant(restaurants)
+    restaurants[Random.rand(restaurants.count)]
+  end
+
+  def restaurants_within(distance)
+    restaurants.select{ |restaurant| restaurant[:distance] <= :distance.to_i }
+  end
+
+  def restaurants
+    [
+      {
+        :name => 'Brimm CC',
+        :type => 'Japanese',
+        :distance => 150,
+      },
+      {
+        :name => "Grill'd",
+        :type => 'Burgers',
+        :distance => 350,
+      },
+      {
+        :name => 'Cinamons',
+        :type => 'Sri Lankan',
+        :distance => 130,
+      },
+      {
+        :name => 'Tom Toon Thai',
+        :type => 'Thai',
+        :distance => 400,
+      },
+      {
+        :name => 'Earl Canteen',
+        :type => 'PORK BELLEH!',
+        :distance => 400,
+      },
+      {
+        :name => 'Purple Peanuts',
+        :type => 'Japanese',
+        :distance => 350,
+      },
+      {
+        :name => 'Goz City',
+        :type => 'Turkish',
+        :distance => 170,
+      },
+      {
+        :name => 'Mr Burger',
+        :type => 'Burgers',
+        :distance => 650,
+      },
+    ]
+  end
 end
 
 Lunch.run!
